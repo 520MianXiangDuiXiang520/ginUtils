8edfab13c98f3e94fb6d88abbe991c8a2dc92e70
520MianXiangDuiXiang520 <15364968962@163.com>
2021-03-07 20:28:28 +0800

    add: 添加 gin 请求转 Websocket 工具

---
+++ websocket/ws.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+type WSHandlerFunc func(ws *websocket.Conn)
+type WSConnWrapper func(ws *websocket.Conn)
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// TransformToWS 将 Gin 的 HTTP 连接转换为 WebSocket 连接
+// 他接受一个 WSHandlerFunc 用来处理具体的 WebSocket 事务。
+func TransformToWS(wsFunc WSHandlerFunc, responseHeader http.Header, wrappers []WSConnWrapper) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		ws, err := upGrader.Upgrade(context.Writer, context.Request, responseHeader)
+		for _, wrapper := range wrappers {
+			wrapper(ws)
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer ws.Close()
+		wsFunc(ws)
+	}
+}
---
+++ websocket/ws_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/robfig/cron/v3"
+	"log"
+)
+
+func ExampleTransformToWS() {
+	go func() {
+		c := cron.New(cron.WithSeconds())
+		_, _ = c.AddFunc("* * * * * *", task)
+		c.Start()
+	}()
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	var router = gin.Default()
+	router.GET("/ws", TransformToWS(ping, nil, []WSConnWrapper{SaveConn}))
+
+	if err := router.Run(":8888"); err != nil {
+		log.Println(err)
+	}
+}
+
+func SaveConn(ws *websocket.Conn) {
+	webSocketConnList = append(webSocketConnList, ws)
+}
+
+var webSocketConnList = make([]*websocket.Conn, 0)
+
+var canvas string
+var canvasVersion int
+
+func ping(ws *websocket.Conn) {
+	for {
+		_, message, err := ws.ReadMessage()
+		fmt.Println(string(message))
+		if err != nil {
+			break
+		}
+		canvas += string(message)
+		canvasVersion++
+	}
+}
+
+var version = 0
+
+func task() {
+	if version == canvasVersion {
+		return
+	}
+	for _, conn := range webSocketConnList {
+		fmt.Println(canvas)
+		err := conn.WriteMessage(websocket.TextMessage, []byte(canvas))
+		if err != nil {
+			break
+		}
+		version = canvasVersion
+	}
+}
